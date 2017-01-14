caece8eef18e0dfba1687453e46089e879adbb2a
d4smart <d4smart@foxmail.com>
2017-01-14 10:53:05 +0000

    Add account activations

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,15 @@ class User extends Model implements AuthenticatableContract,
      */
     protected $hidden = ['password', 'remember_token'];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
+
     /**
      * 返回用户的gravatar头像url
      * @param string $size 头像的大小参数
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = 'd4smart@foxmail.com';
         $user->password = 'password';
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
